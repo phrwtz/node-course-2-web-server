0e02d1042db55394a75eac324da6a9e5d1badfd4
Paul Horwitz <phorwitz@concord.org>
2020-05-31 16:27:04 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(process.cwd() + '/views/partials');
+app.set('view engine', 'hbs');
+
+
+app.use((req, res, next) => {
+ var now = new Date().toString();
+ var log = `${now}; ${req.method} ${req.url}`;
+ console.log(log);
+ fs.appendFile('server.log', log + '\n', (err) => console.log('Unable to append to server.log'))
+ next();
+});
+
+/* app.use((req, res, next) => {
+ res.render('maintenance.hbs');
+}); */
+
+app.use(express.static(process.cwd() + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+ return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (txt) => {
+ return txt.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+ res.render('home.hbs', {
+  pageTitle: 'Home Page',
+  welcomeMessage: 'Welcome to my website!'
+ });
+});
+
+app.get('/about', (req, res) => {
+ res.render('about.hbs', {
+  pageTitle: 'About page'
+ });
+});
+
+app.get('/bad', (req, res) => {
+ res.send({
+  errorMessage: 'Unable to handle request'
+ })
+})
+
+
+
+app.listen(3000, () => {
+ console.log('Server is up on port 3000')
+});
